f74b009c21033e13c8168672d54289de8069c841
Afrim Thaqi <afrimthaqi1401@yahoo.no>
2022-05-04 19:49:09 +0200

    Client clean up and avoid accidental capture of old chatLog

--- client/index.jsx
+++ client/index.jsx
@@ -42,26 +42,12 @@ function ChatApplication({ username }) {
     ws.onmessage = (event) => {
       console.log(event.data);
       const { author, message } = JSON.parse(event.data);
-      setChatLog([...chatLog, { author, message }]);
+      setChatLog((oldState) => [...oldState, { author, message }]);
     };
     setWs(ws);
   }, []);
 
-  const [chatLog, setChatLog] = useState([
-    {
-      author: "Afrim",
-      message: "Fuck you",
-    },
-    {
-      author: "Noman",
-      message: "Haaaanji",
-    },
-    {
-      author: "Khalid",
-      message: "Fuck pg6301",
-    },
-  ]);
-
+  const [chatLog, setChatLog] = useState([]);
   const [message, setMessage] = useState("");
 
   function handleNewMessage(event) {
@@ -92,7 +78,7 @@ function ChatApplication({ username }) {
 }
 
 function Application() {
-  const [username, setUsername] = useState("Afrim");
+  const [username, setUsername] = useState();
 
   if (!username) {
     return <Login onLogin={(username) => setUsername(username)} />;
