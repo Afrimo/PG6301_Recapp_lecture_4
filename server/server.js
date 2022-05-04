9f1e1858172df20f0c9133df1edcd81494002fc2
Afrim Thaqi <afrimthaqi1401@yahoo.no>
2022-05-04 15:05:30 +0200

    Server React from Express

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const application = express();
 
+application.use(express.static("../client/dist"));
+
 const server = application.listen(process.env.PORT || 3000, () => {
   console.log(`http://localhost:${server.address().port}`);
 });
