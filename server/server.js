72e8e563f1db5a1d092e1b67e7eb2a274c5caeaa
Afrim Thaqi <afrimthaqi1401@yahoo.no>
2022-05-04 19:18:52 +0200

    Message received from server (failed??)

--- server/server.js
+++ server/server.js
@@ -1,17 +1,16 @@
 import express from "express";
 import { WebSocketServer } from "ws";
 
-const application = express();
+const app = express();
 
-application.use(express.static("../client/dist"));
+app.use(express.static("../client/dist"));
 
 const wsServer = new WebSocketServer({ noServer: true });
+wsServer.on("connect", (socket) => {
+  socket.send("Hello there");
+});
 
-const server = application.listen(process.env.PORT || 3000, () => {
-  wsServer.on("connect", (socket) => {
-    console.log("ws connected");
-  });
-
+const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`http://localhost:${server.address().port}`);
   server.on("upgrade", (req, socket, head) => {
     wsServer.handleUpgrade(req, socket, head, (socket) => {
